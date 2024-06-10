ca609544dbba15ed4f4c61316213b51e360e6beb
Pasante IT <soporte1@dislorlara.local>
2024-06-10 08:57:51 -0400

    tercer commit

--- crearPersonalFuncion.php
+++ crearPersonalFuncion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include("conexion.php");
@@ -18,24 +17,3 @@ if ($query1) {
 } else {
     echo '<script language="javascript">alert("Error al añadir el personal");</script>';
 }
-=======
-<?php
-
-include("conexion.php");
-
-
-$cargoruta = mysqli_real_escape_string($conexion, $_POST['cargoruta']);
-$area = mysqli_real_escape_string($conexion, $_POST['area']);
-$nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
-
-$sql1="INSERT INTO personal(id_cargoruta,id_area,nombre)
-VALUES('$cargoruta','$area','$nombre')";
-
-$query1 = mysqli_query($conexion, $sql1);
-
-if ($query1) {
-    echo '<script language="javascript">alert("Personal añadido correctamente"); window.location.href = "index.php";</script>';
-} else {
-    echo '<script language="javascript">alert("Error al añadir el personal");</script>';
-}
->>>>>>> 559cab82091e040721218c428ee4f663ae3dd8c8
--- eliminarTlfFuncion.php
+++ eliminarTlfFuncion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include("conexion.php");
@@ -16,22 +15,3 @@ if ($query1) {
 } else {
     echo '<script language="javascript">alert("Error al eliminar los datos");</script>';
 }
-=======
-<?php
-
-include("conexion.php");
-
-$id_telefono=$_GET["id"];
-
-$sql1="UPDATE telefonos SET activo=0 WHERE id_telefono='$id_telefono'";
-$sql2="UPDATE tlf_asignado SET activo=0 WHERE id_telefono='$id_telefono'";
-
-$query1 = mysqli_query($conexion,$sql1);
-$query2 = mysqli_query($conexion,$sql2);
-
-if ($query1) {
-    echo '<script language="javascript">alert("Datos eliminados correctamente"); window.location.href = "index.php";</script>';
-} else {
-    echo '<script language="javascript">alert("Error al eliminar los datos");</script>';
-}
->>>>>>> 559cab82091e040721218c428ee4f663ae3dd8c8
